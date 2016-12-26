cb82396c2eda783feee0da5ed986d475773e900c
Jose Escorche <josesco5@gmail.com>
2016-12-26 19:14:43 -0400

    Display question's answers and comments

--- js/controllers/questions/detail.js
+++ js/controllers/questions/detail.js
@@ -17,6 +17,39 @@ angular.module('questionsApp')
         relatedDiscussion: 1,
         peersInvolved: 6,
         conversations: 3
-      }
+      },
+      answers: [
+        {
+          text: 'Numbers or tingling in your feet should be reported to your doctor at your regular visits',
+          createdAt: '2016-04-14T01:17:58Z',
+          author: {
+            name: 'Patricia',
+            avatarUrl: 'http://lorempixel.com/124/125/business/7/cc'
+          },
+          votes: 8,
+          comments: [
+            {
+              text: 'You should check your feet daily for redness, caluses, cracks or skin breakdown.',
+              createdAt: '2016-04-16T01:17:58Z',
+              author: {
+                name: 'David',
+                avatarUrl: 'http://lorempixel.com/124/125/business/9/cc'
+              },
+              votes: 2
+            }
+          ]
+        },
+        {
+          text: 'When your blood sugar is low, your body gives out signs that you need \
+                food. Different people have different symptoms. You will learn to know \
+                your symptoms.',
+          createdAt: '2016-04-16T01:17:58Z',
+          author: {
+            name: 'Josephs',
+            avatarUrl: 'http://lorempixel.com/124/125/business/9/cc'
+          },
+          votes: -3
+        }
+      ]
     };
   });
